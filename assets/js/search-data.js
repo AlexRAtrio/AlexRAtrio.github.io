3b08f28631665e4af243c8da0dcb6af2bef3d560
AlexRAtrio <33102398+AlexRAtrio@users.noreply.github.com>
2025-05-18 21:22:45 +0000

    Deploying to gh-pages from @ AlexRAtrio/AlexRAtrio.github.io@be26ca73d3e202663119ba1cfbfc157f1fc454fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -369,16 +369,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2.html";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-successfully-defended-my-phd-thesis-regularization-techniques-for-low-resource-machine-translation-grateful-for-the-guidance-support-and-years-of-learning-at-epfl-and-heig-vd",
+          title: '🎉 I successfully defended my PhD thesis, “Regularization Techniques for Low-Resource Machine Translation”!...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
